6875c709a59910fa917e16a544e9b9bba3cc4094
Yusuke Tsutsumi <yusuke@tsutsumi.io>
2016-04-02 00:58:20 -0700

    including swagger statics into web_transmute.

--- ubuild.py
+++ ubuild.py
@@ -1,7 +1,10 @@
 import os
 import subprocess
+import uranium
+from uranium.rules import rule, Once
 
 
+@uranium.task_requires("install_swagger_ui")
 def main(build):
     build.packages.install(".", develop=True)
 
@@ -25,5 +28,30 @@ def distribute(build):
     build.packages.install("wheel")
     build.executables.run([
         "python", "setup.py",
-        "sdist", "bdist_wheel", "--universal", "upload"
+        "sdist", "--universal", "upload"
     ])
+
+
+@rule(Once())
+def install_swagger_ui(build):
+    import io
+    import shutil
+    import tarfile
+    version = "2.1.4"
+    PATH = "https://github.com/swagger-api/swagger-ui/archive/v{0}.tar.gz".format(version)
+    TARGET_PATH = os.path.join(build.root, "web_transmute", "swagger", "static")
+    EXTRACTED_TOP_LEVEL_DIRNAME = "swagger-ui-{0}".format(version)
+    build.packages.install("requests")
+    import requests
+    r = requests.get(PATH, stream=True)
+    stream = io.BytesIO()
+    stream.write(r.content)
+    stream.seek(0)
+    tf = tarfile.TarFile.open(fileobj=stream)
+    tf.extractall(path=TARGET_PATH)
+    # move the files under the top level directory.
+    for name in os.listdir(os.path.join(TARGET_PATH, EXTRACTED_TOP_LEVEL_DIRNAME, "dist")):
+        shutil.move(
+            os.path.join(TARGET_PATH, EXTRACTED_TOP_LEVEL_DIRNAME, "dist", name),
+            TARGET_PATH
+        )
---
+++ web_transmute/swagger/__init__.py
@@ -0,0 +1,26 @@
+import os
+import jinja2
+
+CURDIR = os.path.dirname(__file__)
+
+
+def generate_swagger(swagger_static_root, swagger_json_url):
+    tmpl = get_template("swagger.html")
+    return tmpl.format(
+        swagger_root=swagger_static_root,
+        swagger_json_url=swagger_json_url
+    )
+
+
+def get_swagger_static_root():
+    return os.path.join(CURDIR, "static")
+
+_template_cache = {}
+
+
+def get_template(template_name):
+    if template_name not in _template_cache:
+        template_path = os.path.join(CURDIR, template_name)
+        with open(template_path) as fh:
+            _template_cache[template_name] = jinja2.Template(fh.read())
+    return _template_cache[template_name]
