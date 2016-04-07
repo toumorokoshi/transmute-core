b1f65e215176be015a5949efce0a782d2431c36e
Yusuke Tsutsumi <yusuke@tsutsumi.io>
2016-04-07 11:01:16 -0700

    prepping for new build.

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ for directory in directories:
     data_files.append((directory, files))
 
 setup(name='web-transmute',
-      version='0.0.7',
+      version='0.0.8b',
       description=(
           "a utility library to help provide api route "
           "generation form function signature for web "
