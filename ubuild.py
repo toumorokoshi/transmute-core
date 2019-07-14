5fdada409312e2ab12b3f94fe4bda3273f463bed
Yusuke Tsutsumi <yusuke@tsutsumi.io>
2019-07-14 15:43:39 -0700

    minor: switched local development to use uranium-plus

--- ubuild.py
+++ ubuild.py
@@ -2,70 +2,43 @@ import os
 import shutil
 import sys
 import subprocess
-import uranium
+from uranium import current_build
 from uranium.rules import rule, Once
 
+current_build.packages.install("uranium-plus[vscode]")
+import uranium_plus
 
-@uranium.task_requires("install_swagger_ui")
-def main(build):
-    build.packages.install(".", develop=True)
-    # we install flask to allow testing the example.
-    build.packages.install("flask")
-    build.packages.install("tornado")
-    if sys.version_info > (3, 4):
-        build.packages.install("aiohttp")
-        build.packages.install("pytest-aiohttp")
+current_build.config.update(
+    {
+        "uranium-plus": {
+            "module": "transmute_core",
+            "publish": {"additional_args": ["--release"]},
+            "test": {
+                "packages": ["mock", "pytest-benchmark", "flask", "tornado", "pydantic"]
+            },
+        }
+    }
+)
 
+if sys.version_info > (3, 4):
+    current_build.packages.install("aiohttp")
+    current_build.packages.install("pytest-aiohttp")
 
-@uranium.task_requires("main")
-def test(build):
-    """ execute the unit tests. """
-    build.packages.install("mock")
-    build.packages.install("pytest")
-    build.packages.install("pytest-benchmark")
-    build.packages.install("pytest-cov")
-    build.packages.install("flake8")
-    build.executables.run([
-        "py.test", "--cov", "transmute_core",
-        "transmute_core/tests",
-        "--cov-report", "term-missing"
-    ] + build.options.args)
-    # build.executables.run(["flake8", "transmute_core"])
-
-
-@uranium.task_requires("clean_and_install_swagger_ui")
-def publish(build):
-    """ publish the package itself """
-    build.packages.install("wheel")
-    build.packages.install("twine")
-    build.executables.run([
-        "python", "setup.py",
-        "sdist", "bdist_wheel", "--universal", "--release"
-    ])
-    build.executables.run([
-        "twine", "upload", "dist/*"
-    ])
-
-
-def changelog(build):
-    """ create a changelog """
-    build.packages.install("gitchangelog")
-    changelog_text = subprocess.check_output(["gitchangelog", "HEAD...v0.2.9"])
-    with open(os.path.join(build.root, "CHANGELOG"), "wb+") as fh:
-        fh.write(changelog_text)
+uranium_plus.bootstrap(current_build)
+current_build.tasks.prepend("install_swagger_ui", "main")
+current_build.tasks.prepend("clean_and_install_swagger_ui", "publish")
 
 
 def dev_docs(build):
-    changelog(build)
     build.packages.install("Babel")
     build.packages.install("Sphinx")
     build.packages.install("Sphinx-autobuild")
     build.packages.install("sphinx_rtd_theme")
     build.packages.install("sphinxcontrib-programoutput")
-    return build.executables.run([
-        "sphinx-autobuild", "docs",
-        os.path.join("docs", "_build")
-    ] + build.options.args)[0]
+    return build.executables.run(
+        ["sphinx-autobuild", "docs", os.path.join("docs", "_build")]
+        + build.options.args
+    )[0]
 
 
 @rule(Once())
@@ -77,14 +50,16 @@ def clean_and_install_swagger_ui(build):
     import io
     import shutil
     import tarfile
+
     version = "3.20.9"
     PATH = "https://github.com/swagger-api/swagger-ui/archive/v{0}.tar.gz".format(
-        version)
-    TARGET_PATH = os.path.join(
-        build.root, "transmute_core", "swagger", "static")
+        version
+    )
+    TARGET_PATH = os.path.join(build.root, "transmute_core", "swagger", "static")
     EXTRACTED_TOP_LEVEL_DIRNAME = "swagger-ui-{0}".format(version)
     build.packages.install("requests")
     import requests
+
     r = requests.get(PATH, stream=True)
     stream = io.BytesIO()
     stream.write(r.content)
@@ -94,9 +69,10 @@ def clean_and_install_swagger_ui(build):
         shutil.rmtree(TARGET_PATH)
     tf.extractall(path=TARGET_PATH)
     # move the files under the top level directory.
-    for name in os.listdir(os.path.join(TARGET_PATH, EXTRACTED_TOP_LEVEL_DIRNAME, "dist")):
+    for name in os.listdir(
+        os.path.join(TARGET_PATH, EXTRACTED_TOP_LEVEL_DIRNAME, "dist")
+    ):
         shutil.move(
-            os.path.join(
-                TARGET_PATH, EXTRACTED_TOP_LEVEL_DIRNAME, "dist", name),
-            TARGET_PATH
+            os.path.join(TARGET_PATH, EXTRACTED_TOP_LEVEL_DIRNAME, "dist", name),
+            TARGET_PATH,
         )
