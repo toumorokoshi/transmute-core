4282d082377e522f5e60fe740d0cbe2315f76f50
Yusuke Tsutsumi <yusuke@tsutsumi.io>
2019-12-19 13:13:11 -0800

    Fix a memory leak caused by schematics json schemas (#50)
    Schematics types are instantiated before being used as a key to
    identify whether the json schema has already been extracted. As
    a result, every time the schema is extracted, the previous cached
    value is not used, causing the cache dict to grow indefinitely.
    
    _enforce_instance wasn't an accurate description of the desired
    behavior, so it should be renamed.

--- transmute_core/object_serializers/schematics_serializer.py
+++ transmute_core/object_serializers/schematics_serializer.py
@@ -74,16 +74,16 @@ class SchematicsSerializer(ObjectSerializer):
         if isinstance(model, BaseType):
             return model
 
+        if model in self._models:
+            return self._models[model]
+
         if isinstance(model, ModelMeta):
-            return ModelType(model)
+            self._models[model] = ModelType(model)
 
         if model not in self._models and isinstance(model, tuple):
             self._models[model] = ListType(self._translate_to_model(model[0]))
 
-        if model in self._models:
-            return self._models[model]
-
-        return model
+        return self._models.get(model, model)
 
     @staticmethod
     def _to_key(model):
@@ -92,7 +92,7 @@ class SchematicsSerializer(ObjectSerializer):
         return model
 
     def load(self, model, value):
-        model = _enforce_instance(model)
+        model = _enforce_type_instance_or_model_class(model)
         try:
             context = get_import_context(oo=True)
             model = self._translate_to_model(model)
@@ -108,7 +108,7 @@ class SchematicsSerializer(ObjectSerializer):
             raise SerializationException(str(e))
 
     def dump(self, model, value):
-        model = _enforce_instance(model)
+        model = _enforce_type_instance_or_model_class(model)
         try:
             model = self._translate_to_model(model)
             return model.to_primitive(value)
@@ -118,7 +118,12 @@ class SchematicsSerializer(ObjectSerializer):
     def to_json_schema(self, model):
         if isinstance(model, Serializable):
             model = model.type
-        model = _enforce_instance(model)
+        # ensure that the model is an instance,
+        # as further processing steps required that it is.
+        model = _enforce_type_instance_or_model_class(model)
+        # once the model is an instance of something, there
+        # are several legacy transforms (like taking a tuple of schematics
+        # models) that require a second transformation to a pure schematics model.
         model = self._translate_to_model(model)
         return _to_json_schema(model)
 
@@ -174,11 +179,15 @@ def _dict_type_to_json_schema(dict_type):
     return {"type": "object", "additionalProperties": _to_json_schema(dict_type.field)}
 
 
-def _enforce_instance(model_or_class):
+def _enforce_type_instance_or_model_class(model_or_class):
     """
-    It's a common mistake to not initialize a
-    schematics class. We should handle that by just
-    calling the default constructor.
+    As a broad range of schematics classes and instances are supported, this
+    ensures that those options are normalized to one of the following:
+
+    * an uninstantiated model
+    * an instantiated type
+
+    This enables the rest of the code to only have to handle those two cases.
     """
     if isinstance(model_or_class, type) and issubclass(model_or_class, BaseType):
         return model_or_class()
